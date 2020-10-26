1151a168484989372ed8d70fc051da62aece51f7
Administrator <Administrator@SKCC16N01329.SKCC.NET>
2020-10-26 10:39:42 +0900

    first push test

---
+++ src/com/hello.java
@@ -0,0 +1,20 @@
+package com;
+
+public class hello {
+	
+//	public static void main(String[] args) {
+//		String s;
+//		s = new String("hello world");
+//	    System.out.println(s);
+//	}
+	
+	public static void main(String[] args) {
+		args = new String[2];
+		args[0] = "first";
+		args[1] = "second";
+		for (int i=0; i< args.length; i++) {
+			System.out.println(args[i]);
+		}
+		
+	}
+}
